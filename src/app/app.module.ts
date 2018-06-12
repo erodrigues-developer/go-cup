d193bd044b1baef362463306c6fb3718d238066d
erodrigues-developer <erodrigues.developer@gmail.com>
2018-06-12 20:18:52 -0300

    Ignite UI for Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,40 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuth } from "angularfire2/auth";
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { IgxNavigationDrawerModule } from 'igniteui-angular/navigation-drawer';
+import { IgxButtonModule } from 'igniteui-angular/main';
+import { IgxIconModule } from 'igniteui-angular/main';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDDAa3h1KO4LFHX9MRCNZ8259uztT8DIgk",
+  authDomain: "game-cup-pi.firebaseapp.com",
+  databaseURL: "https://game-cup-pi.firebaseio.com",
+  projectId: "game-cup-pi",
+  storageBucket: "game-cup-pi.appspot.com",
+  messagingSenderId: "468609302013"
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
+    IgxIconModule,
+    IgxButtonModule,
+    IgxNavigationDrawerModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AngularFireModule, AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
